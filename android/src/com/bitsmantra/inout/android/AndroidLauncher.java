18b3cb7705b14daa625925969e0b24828fe77067
NISH1001 <nishparadox@gmail.com>
2015-11-27 14:12:18 +0545

    add libgdx, ashley ECS engine and box2d

---
+++ android/src/com/bitsmantra/inout/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.bitsmantra.inout.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.bitsmantra.inout.InOut;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new InOut(), config);
+	}
+}
---
+++ desktop/src/com/bitsmantra/inout/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.bitsmantra.inout.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.bitsmantra.inout.InOut;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new InOut(), config);
+	}
+}
